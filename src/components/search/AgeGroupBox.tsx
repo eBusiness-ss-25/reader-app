8802671eae84a6e9619f5e41d4a638eb46d2ba88
leon.gauweiler <70015546+Gauweiler42@users.noreply.github.com>
2025-06-30 17:43:33 +0200

    feat: Added an age search

--- src/components/search/AgeGroupBox.tsx
+++ src/components/search/AgeGroupBox.tsx
@@ -1,5 +1,6 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function AgeGroupBox() {
   return (
@@ -7,13 +8,19 @@ export default function AgeGroupBox() {
       <h2 className="text-2xl font-light">Welche Altersgruppe?</h2>
       <div className="gap-4 grid grid-cols-3">
         <AspectRatio ratio={1 / 1} className="bg-muted rounded-3xl">
-          <Button variant="outline" className="w-full h-full text-8xl bg-primary-muted">👶🏻</Button>
+          <Link href="/search/age/0">
+            <Button variant="outline" className="w-full h-full text-8xl bg-primary-muted">👶🏻</Button>
+          </Link>
         </AspectRatio>
         <AspectRatio ratio={1 / 1} className="bg-muted rounded-3xl">
-          <Button variant="outline" className="w-full h-full text-8xl bg-primary-muted">🛝</Button>
+          <Link href="/search/age/4">
+            <Button variant="outline" className="w-full h-full text-8xl bg-primary-muted">🛝</Button>
+          </Link>
         </AspectRatio>
         <AspectRatio ratio={1 / 1} className="bg-muted rounded-3xl">
-          <Button variant="outline" className="w-full h-full text-8xl bg-primary-muted">🎓</Button>
+          <Link href="/search/age/6">
+            <Button variant="outline" className="w-full h-full text-8xl bg-primary-muted">🎓</Button>
+          </Link>
         </AspectRatio>
       </div>
     </div>
