d5c597225eee82066da5c2ac8b443fe4599177a9
leon.gauweiler <70015546+Gauweiler42@users.noreply.github.com>
2025-06-30 14:15:58 +0200

    feat: Added a back button for the category search page

--- src/app/search/category/[categoryId]/page.tsx
+++ src/app/search/category/[categoryId]/page.tsx
@@ -4,6 +4,7 @@ import { use, useState, useEffect } from "react";
 import CategoryAPI, { Category } from '../../../../lib/api/category/category';
 import Loading from "@/components/common/Loading";
 import { BookCard } from "@/components/catalog/BookCard";
+import BackButton from "@/components/common/BackButton";
 
 interface CategorySearchProps {
   params: Promise<{
@@ -38,6 +39,9 @@ export default function CategorySearch({ params }: CategorySearchProps) {
   
   return (
     <div className="p-8">
+      <div className="w-full justify-between mb-4">
+        <BackButton url="/search" />
+      </div>
       <h1 className="text-3xl font-bold mb-4"><span>{ category?.icon || "❓" }</span> { category?.name }</h1>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3">
         {category.Books && category.Books.length > 0 ? (
